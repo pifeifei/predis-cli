b94d93fb1db99947f881bcdef847dfb7be43ee7c
kislong <pifeifei1989@qq.com>
2019-07-28 22:13:31 +0800

    添加 phar 生成

---
+++ bin/predis-cli.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+
+rem --------------------------------------------------------------
+rem 命令行 for Windows. 如果不对,请自行修改 php.exe 路径到 PATH 目录
+rem --------------------------------------------------------------
+
+@setlocal
+
+set PWD_PATH=%~dp0
+
+if "%PHP_PATH%" == "" set PHP_PATH=php.exe
+
+"%PHP_PATH%" "%PWD_PATH%predis-cli" %*
+
+@endlocal
